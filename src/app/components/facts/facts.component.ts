42c6d23db5f132eaae19062c322dce36330c317a
Cedricgoubil <tino22goubil@hotmail.fr>
2021-09-16 21:41:16 +0200

    Facts

--- src/app/components/facts/facts.component.ts
+++ src/app/components/facts/facts.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./facts.component.css']
 })
 export class FactsComponent implements OnInit {
-
+  count: number = 6301368500;
+  stop: boolean
   constructor() { }
 
   ngOnInit() {
   }
+
+  projectcountstop: any = setInterval(() => {
+    this.count++;
+
+    if (this.count === 6301369863) {
+      clearInterval(this.projectcountstop);
+    }
+
+  }, 0.1)
 
 }
